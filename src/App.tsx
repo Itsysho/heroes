2cd3246e126f52d1f74652e4dc05438147faeb75
Ho, Yi-Shin <itisysho@gmail.com>
2021-12-11 16:16:13 +0800

    feat: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { routePath } from "./common/appConfig";
+import Heros from "./containers/hero/Heros";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path={routePath.hero.base} element={<Heros />} />
+          <Route path="*" element={<Heros />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
